2b5a9616b8e2dd6e127717dd244d1f79ea62a059
Zahran03 <zalfarizi502@gmail.com>
2024-07-27 08:24:21 +0700

    final push

---
+++ app/src/main/java/com/example/thenewsapp/api/RetrofitInstance.kt
@@ -0,0 +1,31 @@
+package com.example.thenewsapp.api
+
+import com.example.thenewsapp.util.Constants.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.OkHttpClient
+
+class RetrofitInstance {
+
+    companion object{
+
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+
+        val api by lazy {
+            retrofit.create(NewsAPI::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "d8ff51fc3edf4d63bcf1e9054690e36b"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
